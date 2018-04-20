6f44845f30a9d60cb6812914bd43a1a8094b3d58
Xavier Nayrac <xavier.nayrac@gmail.com>
2018-04-20 09:19:07 +0200

    Add BonusMalus manager

---
+++ js/bonus_malus.js
@@ -0,0 +1,76 @@
+class BonusMalus extends Phaser.GameObjects.GameObject {
+
+  // pool - A Phaser.Physics.Arcade.Group
+  constructor(pool) {
+    super(config.scene[0], "bonusMalus");
+    this.pool = pool;
+
+    this.scene.events.on("bonusmalushit", this.bonusMalus, this);
+  }
+
+  createFrom(staticBrick) {
+    let brick = this.pool
+      .create(staticBrick.x, staticBrick.y, staticBrick.getData("type"))
+      .setGravityY(50);
+
+    let rnd = Math.random();
+
+    brick.setData("bonus", false);
+    brick.setData("malus", false);
+
+    let threshold = 0.0;
+
+    let bonuses = this.scene.levelManager.level.bonuses;
+
+    for (let i = 0; i < bonuses.length; i++) {
+      if (rnd < threshold + bonuses[i].ratio) {
+        brick.setTint(0x00ff00);
+        brick.setData("bonus", true);
+        brick.setData("bonusType", bonuses[i].type);
+        brick.setData("bonusValue", bonuses[i].value);
+        return;
+      } else {
+        threshold += bonuses[i].ratio;
+      }
+    }
+
+    let maluses = this.scene.levelManager.level.maluses;
+
+    for (let i = 0; i < maluses.length; i++) {
+      if (rnd < threshold + maluses[i].ratio) {
+        brick.setTint(0xff0000);
+        brick.setData("malus", true);
+        brick.setData("malusType", maluses[i].type);
+        brick.setData("malusValue", maluses[i].value);
+        return;
+      } else {
+        threshold += maluses[i].ratio;
+      }
+    }
+  }
+
+  bonusMalus(item) {
+    if (item.getData("bonus")) {
+      this._bonus(item);
+    } else {
+      this._malus(item);
+    }
+  }
+
+  _bonus(item) {
+    if (item.getData("bonusType") == "points") {
+      // FIXME score should be an object on its own, like Lives or Paddle.
+      this.scene.score += item.getData("bonusValue");
+      this.scene.updateScore();  
+    } else if (item.getData("bonusType") == "lives") {
+      this.scene.lives.add(item.getData("bonusValue"));
+    }
+  }
+
+  _malus(item) {
+    if (item.getData("malusType") == "lives") {
+      // FIXME One should be able to lost several lives.
+      this.scene.lives.lost();
+    }
+  }
+}
